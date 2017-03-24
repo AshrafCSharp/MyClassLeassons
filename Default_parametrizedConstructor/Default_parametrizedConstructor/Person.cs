942933dd057da9a8da049eba42c0cab7cb695874
AshrafCSharp <linux@almadafcs.com>
2017-03-24 00:56:49 -0400

    DefaultParameterizedConstructor
    فارغ و اخر مرفق معه  Constructor مثال عن كيفية تكوين
    Class متغيرات و ايضاً كيفية تعريف متغير من نوع
    بهذه الانواع Constructor من

---
+++ Default_parametrizedConstructor/Default_parametrizedConstructor/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_parametrizedConstructor
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        // default Constructor .... non Parametrized
+        public Person()
+        {
+            name = "Ashraf";
+            age = 45;
+        }
+
+        // default Parametrized Constructor ....  
+        public Person(string myname, int myage)
+        {
+            name = myname;
+            age = myage;
+        }
+
+        // default Parametrized Constructor ....
+        public Person(string myname2)
+        {
+            name = myname2;
+        }
+ 
+        // Class Method....................................................
+        public void printing()
+        {
+            string msg;
+            if (age == 0)
+            {
+                msg = "you Don't put an Age !";
+                Console.WriteLine($"{name} , {msg}");
+            }
+            else
+                Console.WriteLine($"{name} , {age}");
+
+
+        }
+
+    }
+}
---
+++ Default_parametrizedConstructor/Default_parametrizedConstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_parametrizedConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aaa = "AYHAM";
+            int ggg = 10;
+
+            Person myperson = new Person();
+            Person myperson1 = new Person(aaa,ggg);
+            Person myperson2 = new Person("Taym");
+
+            myperson.printing();// the call method
+
+            myperson.name = "Mohamed";
+            myperson.age = 15;
+            myperson.printing();  // the same call method but we change the properties name & age 
+
+            myperson1.printing();
+
+            myperson2.printing();
+
+            Console.ReadKey();
+
+        }
+    }
+}
