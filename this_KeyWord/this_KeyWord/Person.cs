c10f11c29762dbb0077133b44cb2f1ea92d5915a
AshrafCSharp <linux@almadafcs.com>
2017-03-24 00:58:19 -0400

    this
    Calss في عملها داخل  this مفهوم الكلمه

---
+++ this_KeyWord/this_KeyWord/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_KeyWord
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person (string name,int age)   // constructor 
+        {
+            this.name = name;    //تعني انه اذا استخدمت نفس المتغير في دالة البناء و تريد ان تفهم الكومبايلر عدم الخلط بينهما  this 
+                                //قبل متغير الكلاس لكي يفهم الكومبايلر هذا this  فإنك يجب ان تضع الكلمه المحجوزه 
+            this.age = age;
+        }
+
+        public Person(string name)   // constructor 
+        {
+            this.name = name;
+            
+        }
+
+        public void printme() { Console.WriteLine($"{name} ,{age}"); }
+        
+    }
+}
---
+++ this_KeyWord/this_KeyWord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_KeyWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myperson = new Person("Ashraf", 45);
+            Person myperson1 = new Person("Taym");
+            myperson.printme();
+
+            myperson1.printme();
+
+            Console.ReadKey();
+
+        }
+    }
+}
