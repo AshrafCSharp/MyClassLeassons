1c74ac046065e1497229d40213dc09c823f0d407
AshrafCSharp <linux@almadafcs.com>
2017-03-24 00:52:28 -0400

    WhatIsClass
    Class مثال عن كيفية تكوين

---
+++ WhatIsClass/WhatIsClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle myVehicle = new Vehicle();
+            myVehicle.manufacturer="Toyota";
+            myVehicle.model = "Camry";
+            myVehicle.color = "Blue";
+            myVehicle.enginSize = 4;
+            myVehicle.painting();
+            Console.WriteLine();
+            myVehicle.movement();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WhatIsClass/WhatIsClass/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsClass
+{
+    class Vehicle
+    {
+        public string manufacturer;
+        public string model;
+        public string color;
+        public int enginSize;
+
+        public Vehicle()  // This is the Constructor  ..... it must be the same CLASS name .
+        {
+
+        }
+        public void painting()
+        {
+            Console.WriteLine("I'm Painting Method from Vehicle Class !!!");
+        }
+        public void movement()
+        {
+            Console.WriteLine("I'm movement Method from Vehicle Class !!!");
+        }
+
+    }
+}
